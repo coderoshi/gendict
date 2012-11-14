879919f1193da1c0c844bc3fa811468734bd3d3b
jimbojw <wilson.jim.r@gmail.com>
2012-11-14 17:42:47 -0500

    greatly expanding definition cleanup

--- scripts/generate-images.rb
+++ scripts/generate-images.rb
@@ -4,6 +4,21 @@ require 'yaml'
 
 include Magick
 
+#Procedure
+# start with term
+# extract definitions by part of speech
+# generate metadata sequence of readable 'slides'
+#  - word by itself
+#  - word with first part of speech 
+#  - word with first definition in first part of speech
+#  - word with second definition in first part of speech ...
+#  - word with second part of speech
+#  - word with first def in second part of speech ...
+#  - word by itself
+#  - credits & licensing
+# generate SubRip format closed caption file
+# generate audio for each slide
+
 # Read input term from command line
 term = ARGV[0]
 
@@ -64,6 +79,126 @@ end
 # create slides
 defs = extract_defs(term)
 `mkdir -p terms/#{term}`
+
+slides = []
+slides.push({
+  'display' => term,
+  'script' => term + '.'
+})
+
+defs.each { |kind, definitions|
+  
+  slides.push({
+    'display' => term + "\n(" + kind + ")",
+    'script' => 'Part of speech: ' + kind
+  })
+  
+  puts "\n\n\n=== " + kind + " ===\n\n"
+  
+  i = 0
+  for definition in definitions
+    
+    keep = true
+    
+    # ignore certain definitions
+    keep = keep && (definition =~ /^{{(?:archaic|obsolete|dated)/).nil? #old
+    #keep = keep && (definition =~ /^{{[A-Z]\w+/).nil? # country-specific
+    #keep = keep && (definition =~ /^{{chiefly\|/).nil? # country-specific
+    keep = keep && (definition =~ /^{{rf\w-(?:def|sense)/).nil? # unclear
+    keep = keep && (definition =~ /{{rf(?:ex|def)[\|}]/).nil? # unclear
+    
+    if keep
+      
+      i += 1
+      
+      # convert wikitext into displayable definition
+      display = definition
+      
+      # prefixes
+      display = display.gsub(/^{{(in|un)?(transitive|formal|countable)[^\}]*}}/, '')
+      display = display.gsub(/^{{([\w ]+)}}/, '(\1)')
+      display = display.gsub(/^{{([\w ]+)\|chiefly\|[^\}]+}}/, '(\1)')
+      display = display.gsub(/^{{([^\|\}]+ of)\|([^\}]+)}}/, '\1 "\2"')
+      display = display.gsub(/^{{(\w+)\|_\|(\w+)}}/, '(\1 \2)')
+      display = display.gsub(/^{{(computing|slang)(?:\|.*?)?}}/, '(\1)')
+      display = display.gsub(/^{{senseid\|(?:[^\|]+\|)*([^}]+)}}/, '(\1)')
+      display = display.gsub(/^{{(?:non-gloss definition|n-g)\|(.*?)}}/, '\1')
+      
+      # identifyable templates
+      display = display.gsub(/{{([A-Z][^\|\}]+)[^\}]*}}/, '(\1)')
+      display = display.gsub(/{{context(\|(in|un)?(formal|transitive))*}}/, '')
+      display = display.gsub(/{{context\|(.*?)}}/, '(\1)')
+      display = display.gsub(/{{taxlink\|([^\|]+)\|([^\|]+)}}/, '\1 \2')
+      display = display.gsub(/{{(?:defdate|transitive|tritaxon)\|.*?}}/, '')
+      display = display.gsub(/{{qualifier\|(.*?)}}/, '(\1)')
+      
+      # unidentifyable prefixes
+      display = display.gsub(/^{{([^}]+)}}/, '(\1)')
+      
+      # link syntax
+      display = display.gsub(/\[\[([^\]\|]+)\]\]/, '\1')
+      display = display.gsub(/\[\[(?:[^\]\|]+\|)+([^\]]+)\]\]/, '\1')
+      
+      # emphasis
+      display = display.gsub(/'''([\w ]+)'''/, '"\1"')
+      display = display.gsub(/''(\w+)''/, '\1')
+      display = display.gsub(/('+)([^']+)\1/, '\2')
+      
+      # punctuation and spacing
+      display = display.gsub(/,(?:\s*,)+/, ',')
+      display = display.gsub(/\|/, ', ')
+      display = display.gsub(/\s+/, ' ')
+      display = display.gsub(/^\s+|\s+$/, '')
+      display = display.gsub(/\.*$/, '.')
+      display = display.gsub(/\.+$/, '.')
+      
+      # capitalization
+      display = display[0].upcase + display[1..-1]
+      
+      if !(display =~ /[\{\}\[\]]/).nil?
+        puts '*****************************************************************'
+        puts '* ' + definition
+        puts '* ' + display
+        puts '*****************************************************************'
+      else
+        puts display
+      end
+      
+      # convert wikitext into speakable script
+      script = definition
+      script = ''
+      
+      slides.push({
+        'display' =>
+          term + " (" + kind + ")\n" +
+          i.to_s + ". " + display,
+        'script' =>
+          'Definition ' + i.to_s + ': ' + script
+      })
+      
+    end
+    
+  end
+  
+  if i == 0
+    slides.pop
+  end
+  
+}
+
+puts "\n\n\n"
+
+#https://en.wiktionary.org/w/api.php?action=query&prop=imageinfo&titles=File:en-us-{#term}.ogg&iiprop=url&format=json
+#{"query":{"pages":{"-1":{"ns":6,"title":"File:en-us-tear-verb.ogg","missing":"","imagerepository":"shared","imageinfo":[{"url":"https:\/\/upload.wikimedia.org\/wikipedia\/commons\/b\/b2\/En-us-tear-verb.ogg","descriptionurl":"https:\/\/commons.wikimedia.org\/wiki\/File:En-us-tear-verb.ogg"}]}}}}
+
+slides.push({
+  'display' => term,
+  'script' => term + '.'
+})
+
+#puts slides.to_yaml
+
+exit
 
 # for each part of speech, create a start slide
 defs.each { |kind, definitions|
