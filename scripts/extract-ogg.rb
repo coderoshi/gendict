f99dcc8e3d9adaeabcde00ba548c8eb013f3d656
jimbojw <wilson.jim.r@gmail.com>
2012-11-14 17:42:20 -0500

    extract now correctly extracts all known en-us pronunciations

--- scripts/extract-ogg.rb
+++ scripts/extract-ogg.rb
@@ -1,11 +1,18 @@
+#!ruby
+# extract a sorted list of ogg files from the imagelinks SQL
+# 
+# Ex:
+#  cat dumps/enwiktionary-latest-imagelinks.sql | ruby scripts/extract-ogg.rb
+# 
 
-i = 0
+audio_files = {}
 
-while chunk = gets
-  puts chunk.scan(/'[Ee]n-us-.+?\.ogg/)
-  i += 1
-  if (i>10)
-    exit 1
-  end
-end
+STDIN.read.scan(/'([Ee]n-us-[^']+\.ogg)'/) { |match|
+  k = match[0]
+  k = k[0].upcase + k[1..-1]
+  audio_files[k] = true
+}
+
+puts audio_files.keys.sort
+
 
