530aeef084cb99a785aa184f2c21e569efa84f7b
jimbojw <wilson.jim.r@gmail.com>
2012-11-15 06:12:26 -0500

    adding script to extract just unique english words from the definitions tsv file

---
+++ scripts/extract-english-words.rb
@@ -0,0 +1,19 @@
+#!ruby -n
+# extract unique english words from the latest dumps tsv file
+# 
+# Ex:
+#  cat dumps/enwikt-defs-latest-en.tsv | ruby scripts/extract-english-words.rb > dumps/english-words.txt
+# 
+
+last = nil
+
+while line = gets
+  if !(line =~ /^English\t[a-z]/).nil?
+    term = line.gsub(/^English\t([a-z][^\t]*).*/, '\1')
+    if term != last
+      puts term
+      last = term
+    end
+  end
+end
+
