4fc3e9e28cb4b921a6f39fd7d8cc4c906ba3395b
jimbojw <wilson.jim.r@gmail.com>
2012-11-19 09:36:00 -0500

    adding more MW api notes

--- scripts/generate-images.rb
+++ scripts/generate-images.rb
@@ -460,8 +460,13 @@ slides.push({
   'script' => term + '.'
 })
 
-#https://en.wiktionary.org/w/api.php?action=query&prop=imageinfo&titles=File:en-us-{#term}.ogg&iiprop=url&format=json
-#{"query":{"pages":{"-1":{"ns":6,"title":"File:en-us-tear-verb.ogg","missing":"","imagerepository":"shared","imageinfo":[{"url":"https:\/\/upload.wikimedia.org\/wikipedia\/commons\/b\/b2\/En-us-tear-verb.ogg","descriptionurl":"https:\/\/commons.wikimedia.org\/wiki\/File:En-us-tear-verb.ogg"}]}}}}
+# Get the images (files) linked from an article:
+# https://en.wiktionary.org/w/api.php?action=query&prop=images&titles=#{term}&format=json
+# {"query":{"pages":{"5446":{"pageid":5446,"ns":0,"title":"tear","images":[{"ns":6,"title":"File:Crying-girl.jpg"},{"ns":6,"title":"File:Wikipedia-logo.png"},{"ns":6,"title":"File:en-us-tear-noun.ogg"},{"ns":6,"title":"File:en-us-tear-verb.ogg"}]}}}}
+
+# For a given image (file), find out its real URL
+# https://en.wiktionary.org/w/api.php?action=query&prop=imageinfo&titles=File:en-us-#{term}.ogg&iiprop=url&format=json
+# {"query":{"pages":{"-1":{"ns":6,"title":"File:en-us-tear-verb.ogg","missing":"","imagerepository":"shared","imageinfo":[{"url":"https:\/\/upload.wikimedia.org\/wikipedia\/commons\/b\/b2\/En-us-tear-verb.ogg","descriptionurl":"https:\/\/commons.wikimedia.org\/wiki\/File:En-us-tear-verb.ogg"}]}}}}
 
 slides.push({
   'term' => term,
