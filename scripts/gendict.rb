02b77319d26a522fd1020ce6245a0772ca03c448
jimbojw <wilson.jim.r@gmail.com>
2012-11-26 17:32:25 -0500

    re-enabling youtube upload as part of end-to-end gendict script

--- scripts/gendict.rb
+++ scripts/gendict.rb
@@ -10,14 +10,14 @@ term = ARGV[0]
 
 term_arg = command_arg(term)
 dist_dir = command_arg("#{DIST_DIR}/#{term}")
-yaml_file = command_arg("#{DIST_DIR}/#{term}/#{term}-slides.yaml")
+yaml_file = command_arg("#{DIST_DIR}/#{term}/#{term}.yaml")
 
 procedure = [
   "ruby scripts/make-slides.rb #{term_arg}",
   "ruby scripts/make-images.rb",
   "ruby scripts/make-audio.rb",
   "ruby scripts/make-video.rb",
-  #"ruby scripts/upload-video.rb"
+  "ruby scripts/upload-video.rb",
 ].join(' | ') + " > #{yaml_file}"
 
 puts "Running procedure: #{procedure}"
