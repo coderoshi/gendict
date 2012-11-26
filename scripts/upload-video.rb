3c8138e071613cd013059d3de024583d5d8ea271
jimbojw <wilson.jim.r@gmail.com>
2012-11-26 17:17:59 -0500

    removing definition generation from upload-video script; that now occurs in the make-slides script

--- scripts/upload-video.rb
+++ scripts/upload-video.rb
@@ -3,33 +3,23 @@
 # Takes a presentation serialized as YAML from STDIN for which a video has
 # already been generated and uploads it to YouTube.
 # 
+# TODO:
+#  - record the YouTube ID from the upload and add it to the presentation object
+#  - handle errors that may occur during the upload and add relevent information
+#    to the presentation object.
+#  - gracefully handle missing configuration env variables
+# 
 
 require 'yaml'
 require 'youtube_it'
 
 require './scripts/common.rb'
 
-# builds out a definiton for a given term, if not already specified in the
-# presentation
-def build_definition(presentation)
-  slides = presentation['slides']
-  term = presentation['term']
-  definition = presentation['definition']
-  if definition.nil?
-    definition = slides.map{|slide|
-      term == slide['display'] ? nil : slide['display'].to_s.gsub(/\n/, ' ')
-    }.compact.join("\n")
-    url = "http://en.wiktionary.org/wiki/#{term}"
-    definition += "\n\n" + url
-  end
-  return definition
-end
-
 # upload a video given a client connection and a presentation data object
 def upload_video(client, presentation)
   slides = presentation['slides']
   term = presentation['term']
-  definition = build_definition(presentation)
+  definition = presentation['definition']
   client.video_upload(
     File.open(presentation['video']),
     :title => term,
@@ -45,4 +35,6 @@ client = YouTubeIt::Client.new(
 
 presentation = YAML::load(STDIN.read)
 upload_video(client, presentation)
+
+puts presentation.to_yaml
 
