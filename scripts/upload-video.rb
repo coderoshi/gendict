714f1dc42f0ef71a210c73e0ee65fbe32d515928
jimbojw <wilson.jim.r@gmail.com>
2012-11-26 17:00:36 -0500

    updating upload-video to grab video location from presentation object

--- scripts/upload-video.rb
+++ scripts/upload-video.rb
@@ -1,32 +1,48 @@
+#!ruby
+# 
+# Takes a presentation serialized as YAML from STDIN for which a video has
+# already been generated and uploads it to YouTube.
+# 
+
+require 'yaml'
 require 'youtube_it'
 
-YOUTUBE_UN = ENV['YOUTUBE_UN'] || "viddictionary"
-YOUTUBE_PW = ENV['YOUTUBE_PW']
-YOUTUBE_DK = ENV['YOUTUBE_DK']
+require './scripts/common.rb'
 
-client = YouTubeIt::Client.new(
-  :username => YOUTUBE_UN,
-  :password =>  YOUTUBE_PW,
-  :dev_key => YOUTUBE_DK)
-
-def build_definition(slides, term)
-  definition = slides.map{|slide|
-    term == slide['display'] ? nil : slide['display'].to_s.gsub(/\n/, ' ')
-  }.compact.join("\n")
-  url = "http://en.wiktionary.org/wiki/#{term}"
-  definition += "\n\n" + url
+# builds out a definiton for a given term, if not already specified in the
+# presentation
+def build_definition(presentation)
+  slides = presentation['slides']
+  term = presentation['term']
+  definition = presentation['definition']
+  if definition.nil?
+    definition = slides.map{|slide|
+      term == slide['display'] ? nil : slide['display'].to_s.gsub(/\n/, ' ')
+    }.compact.join("\n")
+    url = "http://en.wiktionary.org/wiki/#{term}"
+    definition += "\n\n" + url
+  end
+  return definition
 end
 
-def upload_video(client, slides)
-  term = slides.first['term']
-  definition = build_definition(slides, term)
+# upload a video given a client connection and a presentation data object
+def upload_video(client, presentation)
+  slides = presentation['slides']
+  term = presentation['term']
+  definition = build_definition(presentation)
   client.video_upload(
-    File.open("terms/#{term}.avi"),
+    File.open(presentation['video']),
     :title => term,
     :description => definition,
     :keywords => [term, 'definition'])
 
 end
 
+client = YouTubeIt::Client.new(
+  :username => ENV['YOUTUBE_UN'],
+  :password => ENV['YOUTUBE_PW'],
+  :dev_key => ENV['YOUTUBE_DK'])
+
 presentation = YAML::load(STDIN.read)
-upload_video(client, presentation['slides'])
+upload_video(client, presentation)
+
