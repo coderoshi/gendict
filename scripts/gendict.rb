e11f4765204ad587c3e45c6f7ffd58fb6034b0a2
jimbojw <wilson.jim.r@gmail.com>
2012-11-29 20:13:00 -0500

    allowing gendict.rb to accept a list of terms from STDIN rather than the command line

--- scripts/gendict.rb
+++ scripts/gendict.rb
@@ -1,6 +1,7 @@
 #!ruby
 # 
-# Given a term on the command line, generate a video from end-to-end.
+# Given a term on the command line, generate a video from end-to-end, or treat
+# STDIN as a list of terms to generate.
 # 
 
 require 'yaml'
@@ -8,18 +9,35 @@ require './scripts/common.rb'
 
 term = ARGV[0]
 
-term_arg = command_arg(term)
-dist_dir = command_arg("#{DIST_DIR}/#{term}")
-yaml_file = command_arg("#{DIST_DIR}/#{term}/#{term}.yaml")
+if term.nil?
+  terms = STDIN.read.split("\n")
+else
+  terms = [term]
+end
 
-procedure = [
-  "ruby scripts/make-slides.rb #{term_arg}",
-  "ruby scripts/make-images.rb",
-  "ruby scripts/make-audio.rb",
-  "ruby scripts/make-video.rb",
-  "ruby scripts/upload-video.rb",
-].join(' | ') + " > #{yaml_file}"
+index = 0
 
-puts "Running procedure: #{procedure}"
-`mkdir -p #{dist_dir}`
-`#{procedure}`
+terms.each do |term|
+
+  index += 1
+  puts "=============== GENERATING TERM #{index} of #{terms.length}: #{term} ==============="
+
+  term_arg = command_arg(term)
+  dist_dir = command_arg("#{DIST_DIR}/#{term}")
+  yaml_file = command_arg("#{DIST_DIR}/#{term}/#{term}.yaml")
+
+  procedure = [
+    "ruby scripts/make-slides.rb #{term_arg}",
+    "ruby scripts/make-images.rb",
+    "ruby scripts/make-audio.rb",
+    "ruby scripts/make-video.rb",
+    "ruby scripts/upload-video.rb",
+  ].join(' | ') + " > #{yaml_file}"
+
+  puts "Running procedure: #{procedure}"
+  `mkdir -p #{dist_dir}`
+  `#{procedure}`
+
+end
+
+
